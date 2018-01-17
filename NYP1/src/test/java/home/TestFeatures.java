5f753b519c9823f648931f61e67b064d1b4be4f8
mhasan90 <mahmudbd90@gmail.com>
2018-01-17 02:23:27 -0500

    Create NYP1 Module

--- NYP1/src/test/java/home/TestFeatures.java
+++ NYP1/src/test/java/home/TestFeatures.java
@@ -1,16 +1,18 @@
 package home;
 
-import reporting.TestLogger;
 import base.MobileAPI;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
 import pages.MainPage;
+import reporting.TestLogger;
 
 public class TestFeatures extends MobileAPI {
     @Test
     public void home() throws InterruptedException {
-        TestLogger.log("app is launched");
+        TestLogger.log("app1 is launched ...");
         MainPage mainPage = PageFactory.initElements(ad, MainPage.class);
-        mainPage.goToArticles();
+        mainPage.clickMenu();
+        mainPage.clickNews();
+        System.out.println("TC1 Passed");
     }
 }
