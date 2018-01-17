ed73f50903ab5ab08bc4b373b13a3affd3ec3170
mhasan90 <mahmudbd90@gmail.com>
2018-01-17 02:23:55 -0500

    Create UiCatalog Module

--- UiCatalog/src/main/java/pages/UiHome.java
+++ UiCatalog/src/main/java/pages/UiHome.java
@@ -1,4 +1,13 @@
 package pages;
 
-public class UiHome {
+import base.MobileAPI;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class UiHome extends MobileAPI {
+    public void clickBtns(){
+        WebElement btns = ad.findElement(By.xpath("(//XCUIElementTypeStaticText[@name=\"AX error -25205\"])[2]"));
+        btns.click();
+        sleepFor(2);
+    }
 }
--- UiCatalog/src/test/java/iPhone/TestUiHome.java
+++ UiCatalog/src/test/java/iPhone/TestUiHome.java
@@ -1,4 +1,15 @@
 package iPhone;
 
-public class TestUiHome {
+import org.junit.Before;
+import org.testng.annotations.Test;
+import pages.UiHome;
+import reporting.TestLogger;
+
+public class TestUiHome extends UiHome {
+    @Test
+    public void test1(){
+        TestLogger.log("app is launched ...");
+        clickBtns();
+        System.out.println("btns clicked ...");
+    }
 }
